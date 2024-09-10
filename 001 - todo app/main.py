f1764deabd8d50b305b02336a2c82b23814ec9f8
Egormity <kotlaregor9@gmail.com>
2024-09-10 15:37:13 +0700

    day 1-3 done

--- 001 - todo app/main.py
+++ 001 - todo app/main.py
@@ -1,8 +1,25 @@
-user_prompt = "Enter your todo: "
+# user_prompt = "Enter your todo: "
 
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+# todo1 = input(user_prompt)
+# todo2 = input(user_prompt)
+# todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
+# todos = [todo1, todo2, todo3]
+# print(todos)
+
+todos = []
+while True:
+    user_action = input("Type add, show, or exit: ").strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter your todo: ")
+            todos.append(todo)
+        case "show":
+            for item in todos:
+                print(item)
+        case "exit":
+            break
+        # case whatever:
+        case _:
+            print("Unknown command")
