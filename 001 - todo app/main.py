05b0c01cfc4ca04850e6de159585860db7ee0b2a
Egormity <kotlaregor9@gmail.com>
2024-09-28 11:44:37 +0700

    day 13 done. About comments

--- 001 - todo app/main.py
+++ 001 - todo app/main.py
@@ -17,20 +17,27 @@
 # filenames = [filename.replace('.', '-hi.') for filename in filenames]
 # print (filenames)
 
+# text = """
+# Hello!
+# How are you?
+# """
+# print(text)
+
 todos_path = r"C:\Users\kotla\Desktop\python-megacourse\001 - todo app\todos.txt"
 
 def get_todos():
+    """Read a text file and return the list of to-do items"""
     with open(todos_path, "r") as file:
         todos = file.readlines()
     return todos
 
-
 def add_todo(todo):
+    """Write the to-do item to the text file"""
     with open(todos_path, "a") as file:
         file.writelines(todo)
 
-
 def write_todos(todos):
+    """Write the to-do items list to the text file"""
     with open(todos_path, "w") as file:
         file.writelines(todos)
 
@@ -111,4 +118,5 @@ while True:
     else:
         print("Unknown command")
 
+# BUE!
 print("Bye!")
