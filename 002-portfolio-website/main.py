f98be03a060f581bf111553432388cd25ad74669
Egormity <kotlaregor9@gmail.com>
2024-10-29 12:31:27 +0700

    Day 22. Working with csv data

--- 002-portfolio-website/main.py
+++ 002-portfolio-website/main.py
@@ -1,16 +1,37 @@
-import streamlit as st
+import streamlit
+import pandas
 
-PATH = r"C:\Users\kotla\Desktop\python-megacourse\002-portfolio-website\images"
+PATH = r"C:\Users\kotla\Desktop\python-megacourse\002-portfolio-website"
 
-st.set_page_config(layout="wide")
+streamlit.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2 = streamlit.columns(2)
 
 with col1:
-    st.image(f"{PATH}/photo.png")
+    streamlit.image(f"{PATH}/images/photo.png")
 
 with col2:
-    st.title("My boring App")
-    st.info("Hello, *World!* :sunglasses:")
+    streamlit.title("My boring App")
+    streamlit.info("Hello, *World!* :sunglasses:")
 
-st.write('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+col3, empty_col, col4 = streamlit.columns([1, 0.1, 1])
+
+df = pandas.read_csv(f"{PATH}/data.csv", sep=";")
+
+with col3:
+    for index, row in df.iterrows():
+        if index % 2 == 0: continue
+
+        streamlit.header(row["title"])
+        streamlit.write(row["description"])
+        streamlit.image(f"{PATH}/images/{row['image']}")
+        streamlit.write(f"[Source code]({row['url']})")
+    
+with col4:
+    for index, row in df.iterrows():
+        if index % 2 == 1: continue
+
+        streamlit.header(row["title"])
+        streamlit.write(row["description"])
+        streamlit.image(f"{PATH}/images/{row['image']}")
+        streamlit.write(f"[Source code]({row['url']})")
