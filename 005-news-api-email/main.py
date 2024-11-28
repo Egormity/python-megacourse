373594d8d1ae2cb06c277e559c299a441dea0cfd
Egormity <kotlaregor9@gmail.com>
2024-11-28 15:27:38 +0700

    Day 28 - finish sending email app

--- 005-news-api-email/main.py
+++ 005-news-api-email/main.py
@@ -2,16 +2,19 @@ import requests
 from send_email import send_email
 
 API_KEY = "9dc1d76992314cf692ec5af6a3542ef0"
-URL = "https://newsapi.org/v2/everything?q=tesla&from=2024-10-27&sortBy=publishedAt&apiKey=9dc1d76992314cf692ec5af6a3542ef0"
+topic = "python"
+URL = f"https://newsapi.org/v2/everything?q={topic}&from=2024-10-27&sortBy=publishedAt&language=en&apiKey={API_KEY}"
 
 req = requests.get(URL)
 content = req.json()
 
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:5]:
     body += f"""
+    Subject: New News!
     Title: {article["title"]}
     Description: {article["description"]}
+    url: {article["url"]}
     """
 
 body = body.encode("utf-8")
